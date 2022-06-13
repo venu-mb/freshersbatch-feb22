1e4579cd93c30ca325746d2ca241f4044302c7b0
MBVENUP <venuprasad1997@gmail.com>
2022-06-13 23:48:19 +0530

    push the code to repo using git plug in

---
+++ Eclipse/SampleProj/src/Venu.java
@@ -0,0 +1,12 @@
+public class Venu {
+
+	public static void main(String[] args) {
+		System.out.println("Hello VP");
+		for (int i = 0; i < 4; i++) {
+			System.out.println("hi..Hey");
+			System.out.println("hi..Hey");
+		}
+
+	}
+
+}
